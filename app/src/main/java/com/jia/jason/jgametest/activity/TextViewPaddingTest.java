b4d1ac07ba451e70dad529b33b1ec1eb2b92688a
jiaxin <jiaxin@jiaxindeMacBook-Pro.local>
2016-12-29 18:29:46 +0800

    add

--- app/src/main/java/com/jia/jason/jgametest/activity/TextViewPaddingTest.java
+++ app/src/main/java/com/jia/jason/jgametest/activity/TextViewPaddingTest.java
@@ -2,9 +2,15 @@ package com.jia.jason.jgametest.activity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.SparseArray;
 import android.widget.TextView;
 
 import com.jia.jason.jgametest.R;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by jiaxin on 2016/12/2.
@@ -12,6 +18,7 @@ import com.jia.jason.jgametest.R;
 
 public class TextViewPaddingTest extends BaseActivity {
 
+    public static final String TAG = "TextViewPaddingTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,38 @@ public class TextViewPaddingTest extends BaseActivity {
         tv.setSingleLine(true);
         tv.setEllipsize(TextUtils.TruncateAt.END);
         //tv.setTextColor(0);
+
+
+        for (int i = 0; i < 5; i++) {
+            Log.d(TAG, i+"");
+            for (int j = 0; j < 5; j++) {
+                Log.d(TAG, j+"");
+                if (j == 3) {
+                    break;
+                }
+            }
+        }
+
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < 6; i++) {
+            list.add(i, "a"+i);
+        }
+        list.add(2, "add3");
+        Log.d(TAG, list.get(2));
+        for (String s : list) {
+            Log.d(TAG, s);
+        }
+
+        SparseArray<String> array = new SparseArray<>(10);
+        for (int i = 0; i < 6; i++) {
+            array.put(i, "b"+i);
+        }
+        array.put(3, "put3");
+        array.put(9, "put9");
+        Log.d(TAG, array.size()+"");
+        Log.d(TAG, array.get(9));
+        for (int i = 0; i < array.size(); i++) {
+            //Log.d(TAG, array.get(i));
+        }
     }
 }
